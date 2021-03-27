d88816da3e4c0c264f2724cadaa55339a0b1a38c
Mariya <mariya.podosinova@gmail.com>
2021-03-27 13:43:36 +0000

    add theory for Chaining Method

--- script.js
+++ script.js
@@ -143,8 +143,20 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 // console.log(balance2);
 
 //***Maximum value
-const max = movements.reduce(
-  (acc, mov) => (acc > mov ? acc : mov),
-  movements[0]
-);
-console.log(max);
+// const max = movements.reduce(
+//   (acc, mov) => (acc > mov ? acc : mov),
+//   movements[0]
+// );
+// console.log(max);
+//***The Magic of Chaining Methods
+const eurToUsd = 1.1;
+//***PIPELINE
+const totalDepositsUSD = movements
+  .filter((mov) => mov > 0)
+  .map((mov, i, arr) => {
+    // console.log(arr);
+    return mov * eurToUsd;
+  })
+  // .map(mov => mov * eurToUsd)
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(totalDepositsUSD);
