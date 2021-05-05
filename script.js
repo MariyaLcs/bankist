da09a030753e4baf456de52c2647252218179b77
Mariya Podosinova <mariya.podosinova@gmail.com>
2021-05-05 12:37:13 +0100

    add code example for Remainder Operator

--- script.js
+++ script.js
@@ -240,3 +240,11 @@ btnSort.addEventListener("click", function (e) {
 });
 
 // LECTURES
+//***Remainder Operator
+
+labelBalance.addEventListener("click", function () {
+  [...document.querySelectorAll(".movements__row")].forEach(function (row, i) {
+    if (i % 2 === 0) row.style.backgroundColor = "orangered";
+    if (i % 3 === 0) row.style.backgroundColor = "blue";
+  });
+});
