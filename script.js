e87b245379dd951268e4b2763096b5d1bdcaa137
Mariya Podosinova <mariya.podosinova@gmail.com>
2021-05-06 13:45:10 +0100

    create timer, create fn for tick, clearInterval if timer exist

--- script.js
+++ script.js
@@ -167,14 +167,35 @@ const updateUI = function (acc) {
   // Display summary
   calcDisplaySummary(acc);
 };
-
+const startLogOutTimer = function () {
+  const tick = function () {
+    const min = String(Math.trunc(time / 60)).padStart(2, 0);
+    const sec = String(time % 60).padStart(2, 0);
+    //In each call, print the remaining time to UI
+    labelTimer.textContent = `${min}:${sec}`;
+    //When 0 seconds, stop timer and log out user
+    if (time === 0) {
+      clearInterval(timer);
+      labelWelcome.textContent = "Log in to get started";
+      containerApp.style.opacity = 0;
+    }
+    //Decrese 1s
+    time--;
+  };
+  //Set time to 5 minutes
+  let time = 12;
+  //Call the timer every second
+  tick();
+  const timer = setInterval(tick, 1000);
+  return timer;
+};
 // Event handlers
-let currentAccount;
+let currentAccount, timer;
 
 //Fake always logged in
-currentAccount = account1;
-updateUI(currentAccount);
-containerApp.style.opacity = 100;
+// currentAccount = account1;
+// updateUI(currentAccount);
+// containerApp.style.opacity = 100;
 
 btnLogin.addEventListener("click", function (e) {
   // Prevent form from submitting
@@ -217,6 +238,10 @@ btnLogin.addEventListener("click", function (e) {
     // Clear input fields
     inputLoginUsername.value = inputLoginPin.value = "";
     inputLoginPin.blur();
+
+    //Timer
+    if (timer) clearInterval(timer);
+    timer = startLogOutTimer();
 
     // Update UI
     updateUI(currentAccount);
